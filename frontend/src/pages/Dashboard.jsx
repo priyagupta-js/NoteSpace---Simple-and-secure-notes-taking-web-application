7948babca9daf94b7f6df09c16ecdc5ab7b46b7f
Priya Gupta <priyagupta052k@gmail.com>
2025-12-30 14:36:19 +0530

    update frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -124,9 +124,9 @@ export default function Dashboard() {
           {/* Logo */}
           <div className="flex items-center space-x-3">
             <div className="w-10 h-10 bg-linear-to-br from-purple-500 to-pink-500 rounded-xl flex items-center justify-center shadow-lg">
-              <span className="text-white font-bold text-lg">N</span>
+              <span className="text-white cursor-pointer font-bold text-lg">N</span>
             </div>
-            <h1 className="text-2xl font-bold bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
+            <h1 className="text-2xl font-bold bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text cursor-pointer text-transparent">
               NoteSpace
             </h1>
           </div>
@@ -139,7 +139,7 @@ export default function Dashboard() {
               <Search size={18} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
               <input
                 type="text"
-                placeholder="Search notes..."
+                placeholder="Search by titles..."
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 className="pl-10 pr-4 py-2 bg-gray-100 rounded-full w-64 text-sm focus:ring-2 focus:ring-purple-400"
